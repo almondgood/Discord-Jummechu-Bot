2a6d3bb1ec56ab106eb90cddb8a8643c6ed670cd
AlmondGood <jwt2358@naver.com>
2024-02-05 21:09:10 +0900

    점메추가 구현
    점메추가

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import asyncio
 
 jumme = []
 dosirak = []
-jumme_commands = ["점메추", "점메추가", "도시락"]
+jumme_commands = ["!점메추", "!점메추가", "!도시락"]
 
 intents=discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
@@ -32,7 +32,6 @@ with open("dosirak.txt", "r", encoding="UTF-8") as f:
 @bot.event
 async def on_ready():             
     print(f'{bot.user} 에 로그인하였습니다!')
-    
 
 @bot.command(name="점메추")
 async def jummechu(ctx): 
@@ -42,36 +41,42 @@ async def jummechu(ctx):
 async def jummechu(ctx): 
     await ctx.send(random.choice(dosirak))
     
+    
+
+
+
+# TODO : 파일 입출력으로 변경
 @bot.command(name="점메추가")
 async def jummechuga(ctx):
     await ctx.send("추가하고 싶은 음식 이름을 입력해주세요.")
     await ctx.send("저장하시려면 `메추`, 추가하지 않으려면 `나가기`를 입력해주세요.")
 
+    food_list = []
     while True:
-        food_list = (await bot.wait_for('message')).content.strip().split()
+        await asyncio.sleep(0.5)
+        msg = (await bot.wait_for('message')).content.strip().split()
         
-        if '저장' in food_list:
+        if '저장' in msg:
+            if len(food_list) == 0:
+                await ctx.send("점심 메뉴 추가 리스트가 비어있습니다.")
+                continue
+            
             food_list.remove('저장')
             break
             
-        print(food_list[0])
-        if '나가기' in food_list and food_list[0] in jumme_commands:
+        if '나가기' in msg or msg in jumme_commands:
             await ctx.send("점메추가를 종료합니다.")
             return
         
+        food_list += msg
         
-        send_msg = ""
-        if send_msg == "": send_msg = await ctx.send(f"현재 점메 리스트 : []")
-        else : await send_msg.edit(f"현재 점메 리스트 : {food_list}")
+        logging.info(food_list)
+        await ctx.send(f"현재 점메 리스트 : {food_list}")
     
     jumme.append(food_list)
-    print(food_list) 
+    logging.info(food_list)
     await ctx.send(f"저장되었습니다.")
+        
     
-    
-    
-    
-
-
 
 bot.run(token) #생성한 Bot 객체에 토큰을 넣어 실행
